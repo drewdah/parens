65afb7a57d1950c592b20da5bf9f007176fe93cb
Drew Lawton <email@drewlawton.com>
2015-09-06 19:19:11 -0700

    sass and gulp integrated

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    del = require('del'),
+    publicPath = "public/",
+    srcPath = "src/";
+
+// Default task
+gulp.task('default', ['clean'], function () {
+    gulp.start('styles');
+});
+
+// Develop task
+gulp.task('develop', function () {
+    gulp.start('styles');
+    gulp.start('watch');
+});
+
+// Clean task
+gulp.task('clean', function () {
+    return del([
+        publicPath + 'css/'
+    ]);
+});
+
+// Style preprocessing
+gulp.task('styles', function () {
+    gulp.src(srcPath + '/styles/global.scss')
+        .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+        .pipe(gulp.dest(publicPath + 'css'));
+});
+
+// Watch
+gulp.task('watch', function () {
+    // Watch .scss files
+    gulp.watch(srcPath + 'styles/global.scss', function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.start('styles');
+    });
+
+});
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express    = require('express'),
-    http       = require('http'),
-    path       = require('path'),
-    fs         = require('fs'),
-    app        = express(),
-    router     = express.Router();
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    fs = require('fs'),
+    app = express(),
+    router = express.Router();
 
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
@@ -11,12 +11,10 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 fs.readdirSync('./controllers').forEach(function (file) {
-    if(path.extname(file) == ".js") {
-        route = require('./controllers/' + file);
-        route.controller(app);
-    }
+    route = require('./controllers/' + file);
+    route.controller(app);
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
