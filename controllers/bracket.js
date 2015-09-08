d30306e46debf158ef30787970a2f7a8b60e8582
Drew Lawton <email@drewlawton.com>
2015-09-08 09:03:13 -0700

    added match groups, vertical centering, changed to root controller

--- controllers/bracket.js
+++ controllers/bracket.js
@@ -1,8 +1,6 @@
 var Bracket = require('../models/bracket');
 module.exports.controller = function(app) {
-    // /bracket/ route
-    app.get('/bracket', function(req, res) {
+    app.get('/', function(req, res) {
         res.render('bracket/view', {bracket: Bracket})
     });
-
 }
